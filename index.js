33dc7e40503f4e5757fc6b4479642e23dd4e1059
Mudi Ugbowanko <mudi@renegare.com>
2016-04-21 20:54:26 +0100

    ci ready

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 module.exports = function (gen, co) {
-  return (
-    length => (func => {
-      Object.defineProperty(func, 'length', {
-        get: function() {
-          return length
-        }
-      })
-      return func
-    })(co(gen))
-  )(gen.length)
+  return ((length) => ((func) => {
+    Object.defineProperty(func, 'length', {
+      get: function () {
+        return length
+      }
+    })
+    return func
+  })(co(gen)))(gen.length)
 }
