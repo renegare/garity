1a9919627f98082e9876fa7df8e73c89355168e3
Mudi Ugbowanko <mudi@renegare.com>
2016-04-21 21:54:57 +0100

    add a prewrap interface

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-module.exports = function (gen, co) {
-  return ((args) => ((func) => {
-    Object.defineProperty(func, 'length', {
-      get: function () {
-        return gen.length
-      }
-    })
-    return func
-  })(co.apply(null, args)))
+module.exports = function (co, gen) {
+  if (arguments.length === 1) {
+    return function () {
+      return (args => lengthen(co.apply(null, args), args))
+        (Array.prototype.slice.call(arguments, 0))
+    }
+  }
+
+  return (args => (func => lengthen(co.apply(null, args), args))
+  (co.apply(null, args)))
   (Array.prototype.slice.call(arguments, 0).filter(i => i !== co))
+}
+
+const lengthen = (func, args) => {
+  Object.defineProperty(func, 'length', {
+    get: function () {
+      return args[0].length
+    }
+  })
+  return func
 }
--- spec.js
+++ spec.js
@@ -9,13 +9,22 @@ test('arity of the returned func', () => {
   expect(gen.length).to.eql(3)
   expect(co(gen).length).to.eql(0)
   co = stub().returns(function () {})
-  expect(cg(gen, co).length).to.equal(gen.length)
+  expect(cg(co, gen).length).to.equal(gen.length)
 })
 
 test('extra args', () => {
   const gen = function * (a, b) {}
   const co = stub().returns(function () {})
-  const func = cg(gen, co, 1, 2, 3, 4)
+  const func = cg(co, gen, 1, 2, 3, 4)
   expect(co.firstCall.args).to.eql([gen, 1, 2, 3, 4])
   expect(gen.length).to.eql(2)
+})
+
+test('wrap interface', () => {
+  const gen = function * (a, b) {}
+  const co = stub().returns(function () {})
+  const co2 = cg(co)
+  const func = co2(gen, 1, 2, 3, 4)
+  expect(co.firstCall.args).to.eql([gen, 1, 2, 3, 4])
+  expect(gen.length).to.eql(func.length)
 })
